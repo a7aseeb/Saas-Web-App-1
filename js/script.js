8957997798fc6a517bb91283653ff2fab0d52c6a
Abdul Haseeb <ahaseeb0809@gmail.com>
2021-08-04 22:38:52 +0530

    Cant fix FIND JOBS button for mobile devices.

--- js/script.js
+++ js/script.js
@@ -1,23 +1,4 @@
-
-
-function handleInteraction(evt) {
-    evt.preventDefault();
-    console.log('interacted');
-  }
-
-  document.querySelector(".button-container").addEventListener('touchstart', handleInteraction);
-  document.querySelector(".button-container").addEventListener('click', handleInteraction);
-
 document.querySelector(".button-container").addEventListener("click", () => {
-    let text = document.getElementById("filter-jobs").value;
-    getJobs().then(jobs => {
-        let filteredJobs =  filterJobs(jobs, text);
-        showJobs(filteredJobs);
-        document.getElementById("no-jobs").innerHTML = `<h1 id="no-jobs">Showing ${filteredJobs.length} Jobs</h1>`
-    })
-})
-
-document.querySelector(".button-container").addEventListener("touchstart", () => {
     let text = document.getElementById("filter-jobs").value;
     getJobs().then(jobs => {
         let filteredJobs =  filterJobs(jobs, text);
