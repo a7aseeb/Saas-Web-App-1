87fbf68fe3b5563961d1900ec0fd250e2ce503b5
Abdul Haseeb <ahaseeb0809@gmail.com>
2021-08-04 22:27:03 +0530

    Fixing FIND JOBS button pt2\

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 
 
+function handleInteraction(evt) {
+    evt.preventDefault()
+    console.log('interacted')
+  }
+
+el.addEventListener('touchstart', handleInteraction)
+el.addEventListener('click', handleInteraction)
+
 document.querySelector(".button-container").addEventListener("click", () => {
     let text = document.getElementById("filter-jobs").value;
     getJobs().then(jobs => {
